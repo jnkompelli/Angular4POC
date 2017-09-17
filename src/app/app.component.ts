1df23de9a9495e2ceb66b2cdd7a0c79848822ba3
Jkompelli <jnkompelli@gmail.com>
2017-09-17 17:09:58 +0530

    Initial files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular POC';
+  angularLogo = 'https://angular.io/resources/images/logos/angular2/angular.png';
+  
 }
